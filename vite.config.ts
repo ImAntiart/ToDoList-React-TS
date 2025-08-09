4d2c472a715853e43d3c686fbdd5a66a80919a36
Antiart <r2d2rom@mail.ru>
2025-08-09 11:40:06 +0500

    Версия для деплоя2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ToDoList-React-TS/', 
   plugins: [react()],
    server: {
     open: true, // 🔥 Эта строка откроет браузер автоматически
